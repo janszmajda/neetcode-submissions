01fe1fdacd9831ed54a3b6c63f0223c4a248f43e
Jan Szmajda <jan.szmajda2025@gmail.com>
2026-08-09 03:09:53 -0500

    Add: two-integer-sum-ii - submission-11

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-11.py
@@ -0,0 +1,22 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        """
+        The numbers list is sorted so can take first element and compare with last. 
+        if > goal then need to move right pointer down
+        if < than goal need to move left pointer up
+        """
+        n = 1
+        i = 0
+        for x in range(len(numbers)):
+            # print(numbers[i] + numbers[len(numbers) - n])
+            # print(n)
+            # print(numbers[len(numbers) - n])
+            if numbers[i] + numbers[len(numbers) - n] > target:
+                #move right pointer down then retry
+                n += 1
+            elif numbers[i] + numbers[len(numbers) - n] < target:
+                #move left pointer up
+                i += 1
+            else:
+                return [i + 1, len(numbers) - n + 1]
+        return [0,0]
