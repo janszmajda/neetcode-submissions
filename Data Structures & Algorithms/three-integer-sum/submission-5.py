6eb1777089c799909cf844af5e608bef5971be5d
Jan Szmajda <jan.szmajda2025@gmail.com>
2026-08-11 02:17:27 -0500

    Add: three-integer-sum - submission-5

---
+++ Data Structures & Algorithms/three-integer-sum/submission-5.py
@@ -0,0 +1,21 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        ans = []
+        #goal: use two pointer approach while iterating through the 3rd thing
+        numl = sorted(nums)
+        for i in range(len(numl)):
+            f = i + 1
+            l = len(numl) - 1
+            while f < l:
+                if f != l:
+                    if numl[f] + numl[l] + numl[i] > 0:
+                        l -= 1
+                    elif numl[f] + numl[l] + numl[i] < 0:
+                        f += 1
+                    else:
+                        temp = [numl[f], numl[i], numl[l]]
+                        if temp not in ans:
+                            ans.append(temp)
+                        f += 1
+                        l -= 1
+        return ans
